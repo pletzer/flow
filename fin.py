98b1e99d1eecfd28bc4a516325bf137700c3d43d
Alexander Pletzer <alexander@gokliya.net>
2025-04-24 16:19:15 +1200

    got test case 40 deg running

--- fin.py
+++ fin.py
@@ -3,7 +3,7 @@ FEniCS tutorial demo program: Incompressible Navier-Stokes equations
 for flow around a obstacle using the Incremental Pressure Correction
 Scheme (IPCS).
 
-  u' + u . nabla(u)) - div(sigma(u, p)) = f
+  u' + u . nabla(u) - div(sigma(u, p)) = f
                                  div(u) = 0
 """
 
@@ -13,20 +13,19 @@ from mshr import *
 import numpy as np
 import matplotlib.pyplot as plt
 import utils
-import sys
 
 Lx, Ly = 2.5, 2.0 # domain size
 
 nresolution = 32
 nobstacle = 100
-vmax = 2.0
-dt = 0.1 * np.sqrt(Lx * Ly / nresolution**2)/ vmax # time step
-num_steps = 200 # 5000   # number of time steps
+vinput = 1.0
+Re = 10000 # Reynolds number
+dt = 0.01*(1/Re) # 0.001 * np.sqrt(Lx * Ly / nresolution**2)/ vmax # time step
+tmax = 10000 * dt # max time
+num_steps = int(tmax / dt) # 5000   # number of time steps
 T = num_steps * dt           # final time
-mu = 0.001 # 0.0010518 #  dynamic viscosity of water at 18 deg C #0.001
-rho = 1        # density of water, need to change
-alpha = -10 * np.pi/180
-normThickness = 0.05
+alpha = -40 * np.pi/180
+normThickness = 0.10
 
 # t is thickness
 #xc, yc = utils.NACAFoilPoints(nobstacle, m=0.10, p=0.3, t=0.1)
@@ -36,7 +35,7 @@ xc2 = xc*np.cos(alpha) - yc*np.sin(alpha)
 yc2 = xc*np.sin(alpha) + yc*np.cos(alpha)
 # shift/scale to the right location
 xfoil = xc2 + Lx/4.
-yfoil = yc2 + Ly/2.3
+yfoil = yc2 + 2*Ly/3
 
 # Create mesh
 channel = Rectangle(Point(0, 0), Point(Lx, Ly))
@@ -76,14 +75,11 @@ class ObstacleBoundary(SubDomain):
 
 obstacle_boundary = ObstacleBoundary(xc=xfoil, yc=yfoil)
 
-# Define inflow profile
-inflow_profile = (f'1.0', '0')
-
 # Define boundary conditions
-bcu_inflow = DirichletBC(V, Expression(inflow_profile, degree=2), inflow)
-bcu_walls = DirichletBC(V, Constant((0, 0)), walls)
-bcu_obstacle = DirichletBC(V, Constant((0, 0)), obstacle_boundary)
-bcp_outflow = DirichletBC(Q, Constant(0), outflow)
+bcu_inflow = DirichletBC(V, Constant((vinput, 0)), inflow)
+bcu_walls = DirichletBC(V, Constant((vinput, 0)), walls)
+bcu_obstacle = DirichletBC(V, Constant((0, 0)), obstacle_boundary) # no slip
+bcp_outflow = DirichletBC(Q, Constant(0), outflow) # only for the pressure
 bcu = [bcu_inflow, bcu_walls, bcu_obstacle]
 bcp = [bcp_outflow]
 
@@ -114,8 +110,7 @@ U  = 0.5*(u_n + u)
 n  = FacetNormal(mesh)
 f  = Constant((0, 0))
 k  = Constant(dt)
-mu = Constant(mu)
-rho = Constant(rho)
+reynolds = Constant(Re)
 
 # Define symmetric gradient
 def epsilon(u):
@@ -123,13 +118,13 @@ def epsilon(u):
 
 # Define stress tensor
 def sigma(u, p):
-    return 2*mu*epsilon(u) - p*Identity(len(u))
+    return 2*epsilon(u)/reynolds - p*Identity(len(u))
 
 # Define variational problem for step 1
-F1 = rho*dot((u - u_n) / k, v)*dx \
-   + rho*dot(dot(u_n, nabla_grad(u_n)), v)*dx \
+F1 = dot((u - u_n) / k, v)*dx \
+   + dot(dot(u_n, nabla_grad(u_n)), v)*dx \
    + inner(sigma(U, p_n), epsilon(v))*dx \
-   + dot(p_n*n, v)*ds - dot(mu*nabla_grad(U)*n, v)*ds \
+   + dot(p_n*n, v)*ds - dot(nabla_grad(U)*n/reynolds, v)*ds \
    - dot(f, v)*dx
 a1 = lhs(F1)
 L1 = rhs(F1)
@@ -194,18 +189,6 @@ for n in range(num_steps):
     #[bc.apply(b3) for bc in bcu] # should this be added?????
     solve(A3, u_.vector(), b3, 'cg', 'sor')
 
-    # Plot solution
-    # plot(u_, title='Velocity')
-    # plot(p_, title='Pressure')
-
-    # Save solution to file (XDMF/HDF5)
-    xdmffile_u.write(u_, t)
-    xdmffile_p.write(p_, t)
-
-    # Save nodal values to file
-    timeseries_u.store(u_.vector(), t)
-    timeseries_p.store(p_.vector(), t)
-
     # Update previous solution
     u_n.assign(u_)
     p_n.assign(p_)
@@ -218,11 +201,11 @@ for n in range(num_steps):
     print('-'*30)
     print(f"{n} Lift: {lift:.6f} Drag: {drag:.6f} L/D: {lift/drag:.6f}")
 
-    # Update progress bar
-    #progress.update(t / T)
-    #print('u max:', u_.vector().array().max())
 
-# Hold plot
-#interactive()
+
+# Save solution to file (XDMF/HDF5)
+xdmffile_u.write(u_, 0)
+xdmffile_p.write(p_, 0)
+
 
 
